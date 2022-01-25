2e33a96c2093a77743ce8ed2ad8ecd8c257a583e
Guruparan Giritharan <guru_sig@yahoo.com>
2022-01-25 19:48:57 +0530

    Added safe area

--- App.js
+++ App.js
@@ -3,18 +3,23 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './src/screens/home/HomeScreen';
 import MovieInfoScreen from './src/screens/movieInfo/MovieInfoScreen';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 const MainStack = createNativeStackNavigator();
 
 const App = () => {
 
   return (
+    <SafeAreaView style={{ flex: 1 }} edges={['top', 'left', 'right']}>
       <NavigationContainer>
         <MainStack.Navigator>
-          <MainStack.Screen name="Home" component={HomeScreen} />
+          <MainStack.Screen name="Home" component={HomeScreen} options={{
+            headerShown: false
+          }} />
           <MainStack.Screen name="MovieInfo" component={MovieInfoScreen} />
         </MainStack.Navigator>
       </NavigationContainer>
+    </SafeAreaView>
   );
 };
 
